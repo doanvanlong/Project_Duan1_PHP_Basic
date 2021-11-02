2e9675b4484493bca26eff6164b141cfd61a2c35
doanvanlong <longlongdoan1998@gmail.com>
2021-11-02 21:41:09 +0700

    update

--- content/js/main.js
+++ content/js/main.js
@@ -1,28 +1,35 @@
 $(document).ready(function(){
-    //owlcarousel
+    //owlcarousel tab menu product
       $('.owl-carousel').owlCarousel({
         loop:true,
         margin:10,
         nav:true,
-        autoplay:true,
+        // autoplay:true,
         responsive:{
             0:{
                 items:1
             },
             600:{
-                items:3
+                items:4
             },
             1000:{
-                items:1
+                items:5
             }
         }
       });
+      //class active thanh menu
     $('.menu__items').click(function(){
         if($('.menu__items').hasClass('active')){
             $('.menu__items').removeClass('active');
             $(this).addClass('active');
         }
     })
-
+    //class active tab menu product
+    $('.tab__menu-product-items').click(function() {
+        if($('.tab__menu-product-items').hasClass('active')){
+            $('.tab__menu-product-items').removeClass('active');
+            $(this).addClass('active');
+        }
+    })
     
 })
