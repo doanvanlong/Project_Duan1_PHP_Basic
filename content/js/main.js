91979c54d54e554fbe20ecbf0a155d01a8d93e59
doanvanlong <longlongdoan1998@gmail.com>
2021-11-02 19:50:10 +0700

    commit

--- content/js/main.js
+++ content/js/main.js
@@ -1,8 +1,28 @@
 $(document).ready(function(){
+    //owlcarousel
+      $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        autoplay:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:1
+            }
+        }
+      });
     $('.menu__items').click(function(){
         if($('.menu__items').hasClass('active')){
             $('.menu__items').removeClass('active');
             $(this).addClass('active');
         }
     })
+
+    
 })
