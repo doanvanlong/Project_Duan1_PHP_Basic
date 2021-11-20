e5f55a78f34768405a1fd86def6d481283a8f2da
TanThang <phamtanthang88@gmail.com>
2021-11-20 09:47:18 +0700

    hmm

--- admin/views/tin-tuc/them-danh-muc-tin-tuc.php
+++ admin/views/tin-tuc/them-danh-muc-tin-tuc.php
@@ -1,6 +1,6 @@
 <h3 class="alert bg-secondary text-light heading-cate">Thêm danh mục tin tức</h3>
 <div class="row">
-    <div class="col-4">
+    <div class="col-6">
         <form action="" method="post" id="add-dm-tin-tuc">
             <div class="form-group">
                 <label>Tên danh mục tin tức:</label>
